c91f3a18f9ad257348ae8b427c342dbb76c04610
Akib Sadmanee <akibsadmanee@gmail.com>
2018-11-06 22:19:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,11 @@ prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(chrome_path,chrome_options=chrome_options)    
 
-driver.set_window_size(0,0)
-driver.set_window_position(15000,15000)
-
 print("While entering password nothing may appear on the screen, but be sure to type it right as this option is just for security. ")
 
 user = input("Enter Your Facebook Login Username, Phone Number or Email:  ")
 password = getpass.getpass(prompt='Enter Your Facebook Login Password:  ') 
-wish="Wish You Many Many Happy Returns of The Day :D"
-
-driver=webdriver.Chrome(chrome_path)
+wish="Happy Birthday!! Wish You Many Many Happy Returns of The Day :D"
 
 driver.get("https://www.facebook.com/login")
 driver.find_element_by_id('email').send_keys(user)
